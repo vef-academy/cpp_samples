1da0527ef1ac633b1b9fad91f72a6000a8ba50b5
hung303mc <hung216mc@gmail.com>
2019-04-20 22:19:57 +0700

    update sorting method of pb2 in hw3

--- homework_03/hw3_pb2_removeDuplicateLL/hw3_pb2.cpp
+++ homework_03/hw3_pb2_removeDuplicateLL/hw3_pb2.cpp
@@ -10,28 +10,51 @@
 #include <vector>
 #include <forward_list>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
+
+/* 
+    Remove duplicate by sorting linked list
+*/
+void remove_duplicate_fast(forward_list<int>& lst){
+    // sort linked list
+    lst.sort();
+
+    // remove duplicate
+    for(auto it = lst.begin(); it != lst.end(); ){
+        auto nextNode = next(it);
+        if(nextNode == lst.end()){ // escape loop when reach tail
+            break;
+        }
+        if(*nextNode == *it){ // duplicated
+            lst.erase_after(it);
+        }else{
+            it++;
+        }
+    }
+}
 
 /* 
     Using hash table to check duplicate value in linked list
  */
-void remove_duplicate(forward_list<int>& lst){
-    unordered_map<int, int> umap;
-    umap[*lst.begin()]++; // store head node value in hash
+void remove_duplicate_faster(forward_list<int>& lst){
+    unordered_map<int, bool> umap;
+    umap[*lst.begin()] = true; // store head node value in hash table
 
     for(auto it = lst.begin(); it != lst.end();){
         auto nextNode = next(it);
         if(nextNode == lst.end()){ // escape loop when reach tail
             break;
         }
-        if (umap.find(*nextNode) != umap.end()){
+
+        // Find next value in hash table
+        if (umap.find(*nextNode) != umap.end()){// duplicated
             lst.erase_after(it);  
         }else{
-            umap[*nextNode]++;
+            umap[*nextNode] = true;
             it++;
         }
     }
-
 }
 
 void print(const forward_list<int>& l){
@@ -52,7 +75,8 @@ int main()
     print(l1); 
 
     cout << "after remove duplicate " << endl;
-    remove_duplicate(l1);
+    // remove_duplicate_fast(l1);
+    remove_duplicate_faster(l1);
     print(l1);
 
     return 0;
